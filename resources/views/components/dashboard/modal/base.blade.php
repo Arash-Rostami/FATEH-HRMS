038809e0fa1a2e5e90c7c9840592254b7754933f
Arash-Rostami <56028874+Arash-Rostami@users.noreply.github.com>
2026-02-19 22:37:17 +0000

    Fix modal opening animation and refactor confirmation modal

--- resources/views/components/dashboard/modal/base.blade.php
+++ resources/views/components/dashboard/modal/base.blade.php
@@ -1,21 +1,25 @@
 @props(['title', 'actions' => null])
 
 <div
-    x-data="{ show: @entangle($attributes->wire('model')) }"
+    x-data="{ show: @entangle($attributes->wire('model')), active: false }"
+    x-init="
+        $watch('show', value => {
+            if (value) {
+                setTimeout(() => active = true, 50);
+            } else {
+                active = false;
+            }
+        })
+    "
     class="custom-modal"
-    :class="{ 'active': show }"
+    :class="{ 'active': active }"
     style="display: none;"
     x-show="show"
     x-transition:enter="transition duration-0"
-    x-transition:leave="transition duration-500 delay-500"
+    x-transition:leave="transition duration-1000 delay-1000"
 >
-    <!-- Close Icon -->
-    <div
-        class="modal-close-icon"
-        @click="show = false"
-    ></div>
+    <div class="modal-close-icon" @click="show = false"></div>
 
-    <!-- Content -->
     <div class="custom-modal-content">
         @if($title)
             <h3 class="modal-title">{{ $title }}</h3>
