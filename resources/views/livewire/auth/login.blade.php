d3d418b6465ba51327b2311e5a8b6f191f1b4475
Arash_Rostami_ <arashrostami@time-gr.com>
2026-02-24 13:39:57 +0330

    enhanced the login and developed dual video bg backdrop without video upload

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -103,7 +103,7 @@
                         wire:loading.attr="disabled"
                         wire:target="login">
                     <div class="relative flex items-center justify-center gap-2 w-full">
-                        <span class="material-symbols-rounded text-[22px] " wire:loading.remove
+                        <span class="material-symbols-rounded text-[22px]" wire:loading.remove
                               wire:target="login">login</span>
 
                         <svg  class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
