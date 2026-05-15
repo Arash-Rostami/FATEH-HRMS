4f52ec3893bf91cbed9ec5b9f9b83fac36eaf5c8
Arash-Rostami <56028874+Arash-Rostami@users.noreply.github.com>
2026-05-15 22:14:31 +0000

    feat: enable filament database notifications globally

---
+++ app/Traits/InteractsWithNotifications.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Traits;
+
+use Filament\Notifications\Notification;
+use Filament\Notifications\Actions\Action;
+
+trait InteractsWithNotifications
+{
+    /**
+     * Send a notification with an action that dispatches a Livewire event.
+     *
+     * @param string $title
+     * @param string $body
+     * @param string $eventName The Livewire event to dispatch
+     * @param array $eventData Data to send with the event
+     */
+    public function notifyWithAction(string $title, string $body, string $eventName, array $eventData = []): void
+    {
+        Notification::make()
+            ->title($title)
+            ->body($body)
+            ->success()
+            ->actions([
+                Action::make('view')
+                    ->button()
+                    ->dispatch($eventName, $eventData)
+                    ->close(),
+            ])
+            ->sendToDatabase(auth()->user());
+    }
+}
