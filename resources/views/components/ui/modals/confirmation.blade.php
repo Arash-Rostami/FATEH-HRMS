4a2cea0ff6741d2cdb60729866f36ebcc38c5cc5
Arash-Rostami <56028874+Arash-Rostami@users.noreply.github.com>
2026-06-14 11:21:31 +0000

    UI: Fix livewire confirmation modal execution constraint
    - Reverted changes to original `dialog.blade.php` keeping only the `<template x-teleport="body">` as requested.
    - Updated `resources/views/livewire/dashboard/ths/modal.blade.php` and `resources/views/components/dashboard/modal/occasion.blade.php` wrappers to use `<template x-teleport="body">`.
    - Updated `resources/views/components/ui/modals/confirmation.blade.php` to correctly utilize Livewire v3's `$wire.call()` instead of attempting to trigger non-existent listeners through Alpine's `Livewire.dispatch()`. Also bumped its z-index to 200 so it can appear above the newly teleported components.

--- resources/views/components/ui/modals/confirmation.blade.php
+++ resources/views/components/ui/modals/confirmation.blade.php
@@ -31,7 +31,7 @@
                 }
             })
         "
-        class="custom-modal"
+        class="custom-modal z-[200]"
         :class="{ 'active': active }"
         style="display: none;"
         x-show="show"
@@ -67,21 +67,11 @@
                     </button>
                 </template>
 
-{{--                <template x-if="type === 'livewire'">--}}
-{{--                    <button--}}
-{{--                        type="button"--}}
-{{--                        class="modal-btn modal-btn-confirm"--}}
-{{--                        @click="$wire.call(confirmMethod, confirmParams); show = false"--}}
-{{--                    >--}}
-{{--                        تایید--}}
-{{--                    </button>--}}
-{{--                </template>--}}
-
                 <template x-if="type === 'livewire'">
                     <button
                         type="button"
                         class="modal-btn modal-btn-confirm"
-                        @click="Livewire.dispatch('confirmation-confirmed', { method: confirmMethod, params: confirmParams }); show = false"
+                        @click="$wire.call(confirmMethod, confirmParams); show = false"
                     >
                         تایید
                     </button>
